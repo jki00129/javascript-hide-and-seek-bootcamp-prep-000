90169d86194f4d9b10c41b38d899dafac6780a00
Jeewon Kim <jki00129@gmail.com>
2018-07-16 14:34:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ function deepestChild(){
   */
 }
 
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list');
+      const firstList = rankedLists[0];
+      const secondList = rankedLists[1];
+  let children = firstList.children;
+      let start = 1;
+      
+     for (var i =0; i < children.length; i++){
+  let secondchildren = secondList.children;
+       for(var l=0; l < secondchildren.length; l++){
+         return parseInt(children[l] + n);
+       }
+     }
+}
+
 
